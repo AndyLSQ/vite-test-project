0b9608a7b068655c0a2cc4565c5a80108827f09c
Andy <andrew.eliscu@gmail.com>
2023-01-16 11:06:42 -0600

    Update routing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from "../components/TheWelcome.vue";
+   import MonsterForm from '../components/MonsterForm.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+   <main>
+      <h1>Welcome</h1>
+      <MonsterForm />
+   </main>
 </template>

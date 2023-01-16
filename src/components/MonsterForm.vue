6c28d875ce37d8cce63b801ab4c00f956ad6493c
Andy <andrew.eliscu@gmail.com>
2023-01-16 11:06:23 -0600

    Add monster input form

---
+++ src/components/MonsterForm.vue
@@ -0,0 +1,35 @@
+<script setup>
+	import { ref, reactive } from 'vue';
+	import { useMonsterStore } from '@/stores/monsters';
+
+	const store = useMonsterStore();
+
+	const monster = reactive({
+		name: 'Crunchy',
+		age: 13,
+	});
+</script>
+
+<template>
+	<form>
+		<div class="field">
+			<label for="n">Monster Name</label>
+			<input type="text" id="n" v-model="monster.name" />
+		</div>
+
+		<div class="field">
+			<label for="a">Age</label>
+			<input type="number" id="a" v-model="monster.age" />
+		</div>
+
+		<button @click="store.addMonster(monster)" type="submit">Submit</button>
+	</form>
+
+	<div>
+		<ul>
+			<li v-for="aMonster in store.monsters">
+				<div>{{ aMonster.name }}, {{ aMonster.age }}</div>
+			</li>
+		</ul>
+	</div>
+</template>

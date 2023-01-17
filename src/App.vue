cca63dc08cb734e6f27f9bcff6181ae657f7b075
Andy <andrew.eliscu@gmail.com>
2023-01-17 14:01:39 -0600

    Add dynamic classes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <script setup>
-   import { RouterLink, RouterView } from 'vue-router';
+   import { RouterLink, RouterView, useRoute } from 'vue-router';
+   import { useInterfaceStore } from '@/stores/interface';
+
+   const ui = useInterfaceStore();
+   const route = useRoute();
 </script>
 
 <template>
-   <header>
+   <header v-bind:class="`${route.name} ${ui.menuClass}`">
+      <pre>
+         <code>
+            {{route.name}}
+         </code>
+      </pre>
       <div class="wrapper">
+         {{ ui.menuClass }}
          <nav class="site-menu">
             <RouterLink to="/">Home</RouterLink>
             <RouterLink to="/about">About</RouterLink>
             <RouterLink to="/contact">Contact</RouterLink>
             <RouterLink to="/monsters">Monsters</RouterLink>
          </nav>
+
+         <button @click="ui.toggleMenu()">Toggle menu</button>
       </div>
    </header>
    <main class="outlet">
@@ -24,6 +36,7 @@
    .site-menu {
       display: flex;
       flex-direction: row;
+      flex-wrap: wrap;
    }
 
    .site-menu a {

076b8cfce1028415b945ae6daa9adaab42ff0779
Andy <andrew.eliscu@gmail.com>
2023-01-17 14:02:46 -0600

    Add sample data

--- src/stores/monsters.js
+++ src/stores/monsters.js
@@ -3,7 +3,14 @@ import { defineStore } from 'pinia';
 
 export const useMonsterStore = defineStore('monsters', function () {
    // monsters list
-   const list = reactive([]);
+   const list = reactive([
+      {
+         id: 'asdfsad2343',
+         name: 'Munchy',
+         slug: 'munchy',
+         age: 13,
+      },
+   ]);
 
    // add a monster
    function add(monster) {
